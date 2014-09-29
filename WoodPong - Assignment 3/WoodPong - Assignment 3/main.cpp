3b4eecfa0492423570fc61a4a4b9de0ecd4447f2
WoodyWoodsta <sean@anotherway.co.za>
2014-09-29 07:52:06 +0200

    initial commit :8ball:

---
+++ WoodPong - Assignment 3/WoodPong - Assignment 3/main.cpp
@@ -0,0 +1,16 @@
+//==== The main dude! ====
+
+//== Includes
+#include "stdafx.h"
+
+//== Definitions
+
+using namespace std;
+
+//== Variables
+
+//== Implicits
+
+void main(int argc, char* argv[]) {
+  system("PAUSE");
+}
